41195a787b926e355c6e376326f73fe908cfb7c1
Valber Carreiro <valbercarreiro@gmail.com>
2021-02-18 14:10:22 -0300

    ajustando status de resposta

--- api/rotas/fornecedores/TabelaFornecedor.js
+++ api/rotas/fornecedores/TabelaFornecedor.js
@@ -30,5 +30,11 @@ module.exports = {
                 where: { id: id }
             }
         )
+    },
+
+    remover(id) {
+        return Modelo.destroy({
+            where: { id: id }
+        })
     }
 }
--- api/rotas/fornecedores/index.js
+++ api/rotas/fornecedores/index.js
@@ -4,16 +4,26 @@ const Fornecedor = require('./Fornecedor')
 
 roteador.get('/', async (req, res) => {
     const resultados = await TabelaFornecedor.listar()
+    res.status(200)
     res.send(
         JSON.stringify(resultados)
     )
 })
 
 roteador.post('/', async (req, res) => {
-    const dadosRecebidos = req.body
-    const fornecedor = new Fornecedor(dadosRecebidos)
-    await fornecedor.criar()
-    res.send(JSON.stringify(fornecedor))
+    try {
+        const dadosRecebidos = req.body
+        const fornecedor = new Fornecedor(dadosRecebidos)
+        await fornecedor.criar()
+        res.status(201)
+        res.send(JSON.stringify(fornecedor))
+    } catch (erro) {
+        res.status(400)
+        res.send(JSON.stringify({
+            mensagem: erro.message
+        }))
+    }
+    
 })
 
 roteador.get('/:idFornecedor', async (req, res) => {
@@ -21,10 +31,12 @@ roteador.get('/:idFornecedor', async (req, res) => {
         const id = req.params.idFornecedor
         const fornecedor = new Fornecedor({ id: id })
         await fornecedor.carregar()
+        res.status(200)
         res.send(JSON.stringify(fornecedor))
     } catch (erro) {
+        res.status(404)
         res.send(JSON.stringify({
-            mensagem: erro.mensage
+            mensagem: erro.message
         }))
     }
 })
@@ -36,12 +48,31 @@ roteador.put('/:idFornecedor', async (req, res) => {
         const dados = Object.assign({}, dadosRecebidos, { id: id })
         const fornecedor = new Fornecedor(dados)
         await fornecedor.atualizar()
+        res.status(204)
         res.end()
     }catch(erro) {
+        res.status(400)
         res.send(JSON.stringify({
-            mensagem: erro.mensage
+            mensagem: erro.message
         }))
     }
+})
+
+roteador.delete('/:idFornecedor', async (req, res) => {
+    try{
+        const id = req.params.idFornecedor
+        const fornecedor = new Fornecedor({ id: id })
+        await fornecedor.carregar()
+        await fornecedor.remover()
+        res.status(204)
+        res.end()
+    } catch (erro) {
+        res.status(404)
+        res.send(JSON.stringify({
+            mensagem: erro.message
+        }))
+    }
+    
 })
 
 module.exports = roteador
